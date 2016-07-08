de18156e959627807e34f2ced7d57a9f37727687
Kleer LA <kleer@kleer-sandbox.(none)>
2016-07-08 20:04:02 -0300

    Ok

--- lib/juegoahorcado.rb
+++ lib/juegoahorcado.rb
@@ -3,6 +3,7 @@ class JuegoAhorcado
 		@palabrasecreta = "REFRIGERADOR"
 		@contadorintentos = 6
 		@contadorcoincidencias = 0
+		@letra
 	end
 	
 	def palabrasecreta
@@ -20,4 +21,34 @@ class JuegoAhorcado
 	def longitud
 		return @palabrasecreta.length
 	end
+    def ingresarletra(letra)
+		@letra=letra
+		buscarletra	
+	end
+	def letra
+		return @letra
+	end
+	
+	def buscarletra
+		bolok=true
+		@palabrasecreta.each_char do|l|
+			if l==@letra
+				@contadorcoincidencias += 1
+				bolok=false
+			end
+		end
+		if bolok
+			@contadorintentos -= 1			
+		end
+	end
+	def mensaje
+		if(longitud == @contadorcoincidencias)
+			return "Ganaste"
+		end
+		if(@contadorintentos == 0)
+			return "Perdiste, la palabra secreta es : #{@palabrasecreta}" 
+		end
+		return ""
+	end
+private:buscarletra
 end
